95568278f72a632b92e04d4af0f04c6714dc5503
FOREVER1019 <dcy0910@gmail.com>
2021-12-05 13:53:29 +0800

    updata

--- code_for_submission/longitude_latitude(1).m
+++
@@ -1,9 +0,0 @@
-function distance=longitude_latitude(longitude1,latitude1,longitude2,latitude2)
-longitude1=longitude1*3.14/180;
-latitude1=latitude1*3.14/180;
-longitude2=longitude2*3.14/180;
-latitude2=latitude2*3.14/180;
-temp=sqrt((sin((latitude1-latitude2)/2))^2+cos(latitude1)*cos(latitude2)*(sin((longitude1-longitude2)/2))^2);
-distance=2*asin(temp)*6378.137;
-% R=6378.137;
-% distance=R*acos(cos(latitude1)*cos(latitude2)*cos(longitude1-longitude2)+sin(latitude1)*sin(latitude2));
